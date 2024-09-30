668bc737d161d84c02328be5c11ddd5f9665e792
A1d3n217 <aidenwwood@gmail.com>
2024-09-30 11:24:03 -0600

    Initial Commit

---
+++ MadLibHackwich/MadLibHackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadLibHackwichApp.swift
+//  MadLibHackwich
+//
+//  Created by Wood, Aiden - Student on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MadLibHackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
